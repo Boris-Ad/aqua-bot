c7b686685e2eaa9b00fafa3f0e872c14ac4d1ba5
Boris <adboris@yandex.ru>
2023-09-07 20:37:03 +0300

    d ngrok

--- server/src/telegram/telegram.service.ts
+++ server/src/telegram/telegram.service.ts
@@ -10,12 +10,15 @@ export class TelegramService {
 
   @Start()
   async start(@Ctx() ctx: Context) {
- 
-    await ctx.replyWithPhoto('https://cdn1.epicgames.com/offer/4bc43145bb8245a5b5cc9ea262ffbe0e/EGS_MarvelsSpiderManRemastered_InsomniacGamesNixxesSoftware_S1_2560x1440-73702d11161b29a0b7c40a8b489b1808?h=270&quality=medium&resize=1&w=480',
-    {caption:'Доставка воды',reply_markup:{
-      inline_keyboard: [[{ text: 'Сделать заказ', web_app: { url: 'https://d8bd-188-116-176-42.ngrok-free.app' } }]],
-    }})
-
+    await ctx.replyWithPhoto(
+      'https://cdn1.epicgames.com/offer/4bc43145bb8245a5b5cc9ea262ffbe0e/EGS_MarvelsSpiderManRemastered_InsomniacGamesNixxesSoftware_S1_2560x1440-73702d11161b29a0b7c40a8b489b1808?h=270&quality=medium&resize=1&w=480',
+      {
+        caption: 'Доставка воды',
+        reply_markup: {
+          inline_keyboard: [[{ text: 'Сделать заказ', web_app: { url: 'https://aqua-bot.tw1.ru/' } }]],
+        },
+      },
+    );
   }
 
   // @On('message')
@@ -25,8 +28,8 @@ export class TelegramService {
 
   async sendMessage(address: string) {
     const chatId = this.configService.get('CHAT_ID');
-    this.bot.telegram.sendMessage(chatId, 'Заказ на доставку, адрес: ' + address,{
-     reply_markup:{force_reply: true,input_field_placeholder:'Отправьте ответ'}
+    this.bot.telegram.sendMessage(chatId, 'Заказ на доставку, адрес: ' + address, {
+      reply_markup: { force_reply: true, input_field_placeholder: 'Отправьте ответ' },
     });
   }
 }
